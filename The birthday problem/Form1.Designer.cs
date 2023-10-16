2bf0e793422228fbb8c395a0338cb76e565cf404
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-10-16 11:27:41 -0400

    code

--- The birthday problem/Form1.Designer.cs
+++ The birthday problem/Form1.Designer.cs
@@ -29,82 +29,87 @@
         private void InitializeComponent()
         {
             this.storyoutput = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.choise1 = new System.Windows.Forms.Button();
-            this.choise2 = new System.Windows.Forms.Button();
-            this.choise1output = new System.Windows.Forms.Label();
-            this.choise2output = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.option1 = new System.Windows.Forms.Button();
+            this.option2 = new System.Windows.Forms.Button();
+            this.option1Lable = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // storyoutput
             // 
-            this.storyoutput.BackColor = System.Drawing.Color.Black;
+            this.storyoutput.BackColor = System.Drawing.Color.MediumSpringGreen;
             this.storyoutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.storyoutput.Location = new System.Drawing.Point(83, 9);
+            this.storyoutput.Location = new System.Drawing.Point(87, 9);
             this.storyoutput.Name = "storyoutput";
-            this.storyoutput.Size = new System.Drawing.Size(557, 121);
+            this.storyoutput.Size = new System.Drawing.Size(510, 121);
             this.storyoutput.TabIndex = 0;
+            this.storyoutput.Click += new System.EventHandler(this.storyoutput_Click);
             // 
-            // pictureBox1
+            // imageBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(83, 133);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(557, 220);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.imageBox.ErrorImage = null;
+            this.imageBox.Image = global::The_birthday_problem.Properties.Resources.happy_birthday;
+            this.imageBox.InitialImage = null;
+            this.imageBox.Location = new System.Drawing.Point(87, 68);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(562, 264);
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            this.imageBox.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // choise1
+            // option1
             // 
-            this.choise1.Location = new System.Drawing.Point(101, 388);
-            this.choise1.Name = "choise1";
-            this.choise1.Size = new System.Drawing.Size(75, 23);
-            this.choise1.TabIndex = 2;
-            this.choise1.Text = "1";
-            this.choise1.UseVisualStyleBackColor = true;
-            this.choise1.Click += new System.EventHandler(this.choise1_Click);
+            this.option1.Location = new System.Drawing.Point(101, 388);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(75, 23);
+            this.option1.TabIndex = 2;
+            this.option1.Text = "1";
+            this.option1.UseVisualStyleBackColor = true;
+            this.option1.Click += new System.EventHandler(this.choise1_Click);
             // 
-            // choise2
+            // option2
             // 
-            this.choise2.Location = new System.Drawing.Point(492, 388);
-            this.choise2.Name = "choise2";
-            this.choise2.Size = new System.Drawing.Size(75, 23);
-            this.choise2.TabIndex = 3;
-            this.choise2.Text = "2";
-            this.choise2.UseVisualStyleBackColor = true;
-            this.choise2.Click += new System.EventHandler(this.choise2_Click);
+            this.option2.Location = new System.Drawing.Point(492, 388);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(75, 23);
+            this.option2.TabIndex = 3;
+            this.option2.Text = "2";
+            this.option2.UseVisualStyleBackColor = true;
+            this.option2.Click += new System.EventHandler(this.choise2_Click);
             // 
-            // choise1output
+            // option1Lable
             // 
-            this.choise1output.Location = new System.Drawing.Point(182, 391);
-            this.choise1output.Name = "choise1output";
-            this.choise1output.Size = new System.Drawing.Size(227, 20);
-            this.choise1output.TabIndex = 4;
-            this.choise1output.Click += new System.EventHandler(this.choise1output_Click);
+            this.option1Lable.Location = new System.Drawing.Point(182, 391);
+            this.option1Lable.Name = "option1Lable";
+            this.option1Lable.Size = new System.Drawing.Size(227, 20);
+            this.option1Lable.TabIndex = 4;
+            this.option1Lable.Click += new System.EventHandler(this.choise1output_Click);
             // 
-            // choise2output
+            // option2Label
             // 
-            this.choise2output.Location = new System.Drawing.Point(589, 391);
-            this.choise2output.Name = "choise2output";
-            this.choise2output.Size = new System.Drawing.Size(180, 18);
-            this.choise2output.TabIndex = 5;
-            this.choise2output.Click += new System.EventHandler(this.choise2output_Click);
+            this.option2Label.Location = new System.Drawing.Point(589, 391);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(180, 18);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Click += new System.EventHandler(this.choise2output_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.choise2output);
-            this.Controls.Add(this.choise1output);
-            this.Controls.Add(this.choise2);
-            this.Controls.Add(this.choise1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Lable);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.imageBox);
             this.Controls.Add(this.storyoutput);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -112,11 +117,11 @@
         #endregion
 
         private System.Windows.Forms.Label storyoutput;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button choise1;
-        private System.Windows.Forms.Button choise2;
-        private System.Windows.Forms.Label choise1output;
-        private System.Windows.Forms.Label choise2output;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button option1;
+        private System.Windows.Forms.Button option2;
+        private System.Windows.Forms.Label option1Lable;
+        private System.Windows.Forms.Label option2Label;
     }
 }
 
--- The birthday problem/Form1.cs
+++ The birthday problem/Form1.cs
@@ -23,9 +23,10 @@ namespace The_birthday_problem
         public Form1()
         {
             InitializeComponent();
-            storyoutput.Text = "It is your birthday";
-            choise1.Text = "";
-            choise2.Text = "";
+            storyoutput.Text = "It's your birthday tomorrow but you don't think anything has been planned";
+            option1Lable.Text = "plan something with your freinds";
+            option2Label.Text = "stay home";
+            imageBox.Image = Properties.Resources.happy_birthday;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -35,11 +36,185 @@ namespace The_birthday_problem
 
         private void choise1output_Click(object sender, EventArgs e)
         {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+           
+            else if (page == 4)
+            {
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                page = 8;
+            }
 
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 10;
+            }
+            else if (page == 3)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 10;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+
+            switch (page)
+            {
+                case 1:
+                    storyoutput.Text = "It's your birthday tomorrow but you don't think anything has been planned";
+                    option1Lable.Text = "plan something with your freinds";
+                    option2Label.Text = "stay home";
+
+                    imageBox.Image = Properties.Resources.happy_birthday;
+                    break;
+
+                case 2:
+                    storyoutput.Text = "Your friends are taking you to the fair but you promised your little brother you would go with him first ";
+                    option1.Text = "go with them anyways";
+                    option2.Text = " go home";
+
+
+                    break ;
+
+                case 3:
+                    storyoutput.Text = "Your family forgot your birthday ";
+                    option1Lable.Text = "stay home and pout";
+                    option2Label.Text = " call your friends again";
+                    break;
+
+                case 4:
+                    storyoutput.Text = "You go to the fair and they all leave you behind.Would yo like to play again?";
+                    option1.Text = "yes";
+                    option2.Text = "no";
+                    break;
+
+                case 5:
+                    storyoutput.Text = "Your friends take you aout to eat but while your out your mom calls";
+                    option1Lable.Text = "answer her call";
+                    option2Label.Text = "ignore her call";
+                    break;
+
+                case 6:
+                    storyoutput.Text = "Your mom is waiting and asks where you've been ";
+                    option1Lable.Text = "lie and say you were just outside";
+                    option2Label.Text = "tell the truth";
+
+                    break;
+
+                case 7:
+                    storyoutput.Text = "They didn't actually forget they were planning a surprise party . would you like to play again?";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "No";
+                    break;
+
+                case 8:
+                    storyoutput.Text = "Your mom grounds you for lying.Would u like to play again?";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "no";
+                    break ;
+
+                case 9:
+                    storyoutput.Text = "Your mom tells you to do the dishes and says you'll talk later. would you like to play again? ";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+
+                case 10:
+                    storyoutput.Text = "Your mom tells you to come home ";
+                    option1Lable.Text = "go home";
+                    option2Label.Text = "ignore her ans stay ";
+                    break;
+
+                case 11:
+                    storyoutput.Text = "You get home that night and your dad grounds you for a month. Would u like to play again? ";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+
+                case 12:
+                    storyoutput.Text = "your mom is upset but gives you a present anyways. would you like to play again?";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+
+                case 13:
+                    storyoutput.Text = "You get home at 9 and your mom is so mad she wont even talk to you but your dad says your grounded. would you like to play again?";
+                    option1Lable.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+
+                case 99:
+                    storyoutput.Text = "Thank you for playing";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Close();
+                    break;
+                default:
+                    break;
+            }
+                    
         }
 
         private void choise2_Click(object sender, EventArgs e)
         {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+            }
+            else if (page == 10)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 99;
+            }
+            else if (page == 2)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 99;
+            }
+
 
         }
 
@@ -49,6 +224,16 @@ namespace The_birthday_problem
         }
 
         private void choise1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void storyoutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
